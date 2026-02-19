e8dc95f5b37045fe13e28d510f0322627b9b338d
Harshit Sinha <Harshit.27284@stu.upes.ac.in>
2026-02-19 10:36:41 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    // Read dimensions
+    scanf("%d %d", &rows, &cols);
+
+    int A[100][100], B[100][100], sum[100][100];
+
+    // Read first matrix
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Read second matrix
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Add matrices
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // Print result
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
